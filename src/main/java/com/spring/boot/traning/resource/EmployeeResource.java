7dd0871ab95302e6f8f365ea2bdafe94d60138a7
Durgesh RAI <drai548@gmail.com>
2017-12-31 21:40:31 +0530

    initial commit

--- src/main/java/com/spring/boot/traning/resource/EmployeeResource.java
+++ src/main/java/com/spring/boot/traning/resource/EmployeeResource.java
@@ -1,12 +1,9 @@
 package com.spring.boot.traning.resource;
 
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.FOUND;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,13 +23,13 @@ public class EmployeeResource {
 	private final EmployeeService employeeService;
 
 	@PostMapping(consumes = APPLICATION_JSON_UTF8_VALUE, produces = APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<Employee> createANewEmployee(@RequestBody Employee employee) {
-		return new ResponseEntity<>(employeeService.createEmployee(employee), CREATED);
+	public Employee createANewEmployee(@RequestBody Employee employee) {
+		return employeeService.createEmployee(employee);
 	}
 
 	@GetMapping(consumes = APPLICATION_JSON_UTF8_VALUE, produces = APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<List<Employee>> findAllEmployees() {
-		return new ResponseEntity<>(employeeService.finalAllEmployee(), FOUND);
+	public List<Employee> findAllEmployees() {
+		return employeeService.finalAllEmployee();
 	}
 
 }
